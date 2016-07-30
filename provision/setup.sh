f6d15007418adcd683efc3299fbcb9d711a63e81
Daniel W. Robert <danielwrobert@gmail.com>
2016-07-30 16:10:55 -0400

    Updated guest port. Added '...' after progress messages.

--- provision/setup.sh
+++ provision/setup.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 
-echo "Updating apt-get"
-sudo apt-get update
+echo "Updating apt-get..."
+sudo apt-get update > /dev/null
 
-echo "Installing Git"
+echo "Installing Git..."
 sudo apt-get install -y git-all > /dev/null
 
-echo "Installing Nginx"
+echo "Installing Nginx..."
 sudo apt-get install -y nginx > /dev/null
 
-echo "Updating PHP repository"
+echo "Updating PHP repository..."
 sudo apt-get install -y python-software-properties build-essential > /dev/null
 sudo add-apt-repository -y ppa:ondrej/php5 > /dev/null
 
-echo "Updating apt-get once more"
+echo "Updating apt-get once more..."
 sudo apt-get update > /dev/null
 
-echo "Installing PHP"
+echo "Installing PHP..."
 sudo apt-get install -y php5-common php5-dev php5-cli php5-fpm > /dev/null
 
-echo "Installing PHP extensions"
+echo "Installing PHP extensions..."
 sudo apt-get install -y curl php5-curl php5-gd php5-mcrypt php5-mysql > /dev/null
 
-echo "Preparing MySQL"
+echo "Preparing MySQL..."
 sudo apt-get install -y debconf-utils > /dev/null
 debconf-set-selections <<< "mysql-server mysql-server/root_password password root"
 debconf-set-selections <<< "mysql-server mysql-server/root_password_again password root"
 
-echo "Installing MySQL"
+echo "Installing MySQL..."
 sudo apt-get install -y mysql-server > /dev/null
 
-echo "Configuring Nginx"
+echo "Configuring Nginx..."
 cp /var/www/provision/config/nginx_vhost /etc/nginx/sites-available/nginx_vhost > /dev/null
 ln -s /etc/nginx/sites-available/nginx_vhost /etc/nginx/sites-enabled/
 rm -rf /etc/nginx/sites-available/default
