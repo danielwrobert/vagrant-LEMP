3400c3b4d3853cdce41676d723c3834a0c853889
Jovane <jovane.amaro.pires@gmail.com>
2017-08-27 18:13:28 -0300

    add php extensions required for magento 2

--- provision/setup.sh
+++ provision/setup.sh
@@ -32,7 +32,7 @@ echo "Installing PHP..."
 sudo apt-get install -y php7.0 php7.0-fpm php7.0-mbstring php7.0-xml > /dev/null
 
 echo "Installing PHP extensions..."
-sudo apt-get install -y curl php7.0-mysql > /dev/null
+sudo apt-get install -y curl php7.0-curl php7.0-zip php7.0-intl php7.0-mcrypt php7.0-mysql php7.0-gd > /dev/null
 
 # MySQL
 echo "Preparing MySQL..."
